c36fabb838308a039aa071fc38381104393562b5
GNazar <grnazar@ukr.net>
2012-08-01 13:56:59 +0400

    one project

--- project/src/manager/message/Data.java
+++
@@ -1,7 +0,0 @@
-package manager.message;
-
-import java.io.Serializable;
-//class-label for that mark data field in class "Message"
-abstract public class Data implements Serializable{
-
-}
--- project/src/server/Handler.java
+++
@@ -1,5 +0,0 @@
-package server;
-
-interface Handler{
-	
-} 
